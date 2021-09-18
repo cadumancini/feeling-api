90375a96cbaf848e6cee542ad87154b7a0eebee3
Carlos Mancini <carlos@devsquad.email>
2021-09-18 20:16:40 -0300

    first commit

---
+++ src/main/java/com/br/feelingestofados/feelingapi/FeelingApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.feelingestofados.feelingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/feelingestofados/feelingapi/FeelingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.feelingestofados.feelingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
